299d13c479748b6d42b0fe165be7bfcf19c38208
Miya096jp <miyawaki.yske@gmail.com>
2025-01-10 12:13:43 +0900

    async_all_resolved.js, async_rejected_case.jsを作成し動作チェック。async/await版完成

---
+++ 03.asynchronous/lib/async_all_resolved.js
@@ -0,0 +1,16 @@
+import { run, get } from "./db_util.js";
+
+async function allResolved() {
+  await run(
+    `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+  );
+  console.log("Created");
+  await run(`INSERT INTO books (title) VALUES (?)`, ["book1"]);
+  const row = await get(`SELECT * FROM books WHERE id = ?`, [1]);
+  console.log(`id is ${row.id}`);
+  console.log(row);
+  await run(`DROP TABLE IF EXISTS books`);
+  console.log("Dropped");
+}
+
+allResolved();
---
+++ 03.asynchronous/lib/async_rejected_case.js
@@ -0,0 +1,23 @@
+import { run, get } from "./db_util.js";
+
+async function rejectedCase() {
+  try {
+    await run(
+      `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)`,
+    );
+    console.log("Created");
+    await run(`INSERT INTO books (title) VALUES (?)`, []);
+  } catch (err) {
+    console.log(err);
+  }
+  try {
+    await get(`SELECT * FRM books WHERE id = ?`, [1]);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await run(`DROP TABLE IF EXISTS books`);
+    console.log("Dropped");
+  }
+}
+
+rejectedCase();
