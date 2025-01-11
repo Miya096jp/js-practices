6773cd725b3ffbeae7e2f0e06923c97254d0e25d
Miya096jp <miyawaki.yske@gmail.com>
2025-01-11 15:41:09 +0900

    promise_all_resolved.jsのarrow関数のうち、returnだけを返すものを省略記法に書き換え

--- 03.asynchronous/promise_all_resolved.js
+++ 03.asynchronous/promise_all_resolved.js
@@ -13,9 +13,7 @@ run(
     console.log("Created 'books' table.");
     return run(db, "INSERT INTO books (title) VALUES (?)", ["book1"]);
   })
-  .then(() => {
-    return get(db, "SELECT * FROM books WHERE id = ?", [1]);
-  })
+  .then(() => get(db, "SELECT * FROM books WHERE id = ?", [1]))
   .then((row) => {
     console.log(`id is ${row.id}`);
     return get(db, "SELECT * FROM books WHERE id = ?", [1]);
