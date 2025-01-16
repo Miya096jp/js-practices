0c074193f25d7b7c0b2522bbce83e1bf97f5307b
Miya096jp <miyawaki.yske@gmail.com>
2025-01-16 15:22:12 +0900

    各ファイルのCREATE文におけるスタイル修正（半角スペース抜け)

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL UNIQUE)",
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
     console.log("Created 'books' table.");
     db.run("INSERT INTO books (title) VALUES (?)", [null], (err) => {
