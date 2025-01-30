a6e97e1d780ee619be3f2990412d836f0bc55b1f
Miya096jp <miyawaki.yske@gmail.com>
2025-01-30 15:35:35 +0900

    main.jsの処理をexec関数にまとめる

--- 04.class/src/main.js
+++ 04.class/src/main.js
@@ -8,13 +8,17 @@ const note_repository = await WorkflowManager.initialize(db);
 const argv = minimist(process.argv.slice(1));
 const workflow_manager = new WorkflowManager(argv, note_repository);
 
-try {
-  workflow_manager.listNote();
-  workflow_manager.readNote();
-  workflow_manager.deleteNote();
-  workflow_manager.writeNote();
-} catch (err) {
-  console.error("An unexpected error occured:", err.message);
-  console.error(err.stack);
-  process.exit(1);
+function exec() {
+  try {
+    workflow_manager.listNote();
+    workflow_manager.readNote();
+    workflow_manager.deleteNote();
+    workflow_manager.writeNote();
+  } catch (err) {
+    console.error("An unexpected error occured:", err.message);
+    console.error(err.stack);
+    process.exit(1);
+  }
 }
+
+exec();
